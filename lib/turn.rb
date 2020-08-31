db3e3642a4553fd8faf59b3196d4dbf13e80ce63
Amanda Mongiove <amandamongiove@gmail.com>
2020-08-31 18:11:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,12 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 #display-board
-def display_board
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
+def display_board (board[index])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 display_board
 
