86ef8772bac04b57899babd50b1cd724a3e976d9
Amanda Mongiove <amandamongiove@gmail.com>
2020-08-31 17:56:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,6 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+#display-board
 def display_board
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
@@ -6,3 +9,37 @@ def display_board
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 display_board
+
+#valid-move
+def valid_move?(board, index)
+  if index < 0 || index > 8
+    false
+  else
+    if position_taken?(board,index) == true
+      false
+    else
+      true
+    end
+  end
+end
+
+#position taken
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+#move
+def move(board, index , player_character = "X")
+  board[index] = player_character
+  end
+
+  #input to index
+  def input_to_index(input)
+    input.to_i - 1
+    index = input.to_i - 1
+    return index
+    end
